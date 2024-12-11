5dc3cc7e60127f235abb7ad65f8b85b629d3bbfc
krussell2-rrc <143433171+krussell2-rrc@users.noreply.github.com>
2024-12-10 23:49:16 -0600

    Refined the process for checking images uploaded through post.php

--- post.php
+++ post.php
@@ -33,6 +33,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $imagePath = file_upload_path($_FILES['file']['name']);
     $imageName = pathinfo($_FILES['file']['name'], PATHINFO_FILENAME);
     $imageExtension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+    $new_file_path = file_upload_path($imagePath);
+    $temporary_file_path = $_FILES['file']['tmp_name'];
 
     if(strlen($title) >= 1 && strlen($content) >= 1){
         if(isset($_POST['postType'])){
@@ -40,47 +42,61 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             switch($postType){
                 case "menu":
                 if(isset($_FILES['file']) && $_FILES['file']['error'] === 0){
-                    $menuItemQuery = "INSERT INTO menuitems (item_name, description, cost, category_id) values (:item_name, :description, :cost, :category_id)";
-                    $menuItemStatement = $db->prepare($menuItemQuery);
-                    $menuItemStatement->bindValue(':item_name', $title);
-                    $menuItemStatement->bindValue(':description', $content);
-                    $menuItemStatement->bindValue(':cost', $validated_cost);
-                    $menuItemStatement->bindValue(':category_id', $validated_categoryId);
-                    $menuItemStatement->execute();
-
-                    $menuItemID = $db->lastInsertId();
-
-                    $newPageQuery = "INSERT INTO pages (menuItem_id, page_title) VALUES (:menuItem_id, :page_title)";
-                    $newPageQueryStatement = $db->prepare($newPageQuery);
-                    $newPageQueryStatement->bindValue(':menuItem_id', $menuItemID);
-                    $newPageQueryStatement->bindValue(':page_title', $title);
-                    $newPageQueryStatement->execute();
-
-                    $resized_paths = [
-                        ['image_path' => $imagePath],
-                        ['image_path' => 'uploads/' . $imageName . '_medium' . '.' . $imageExtension],
-                        ['image_path' => 'uploads/' . $imageName . '_thumbnail' . '.' . $imageExtension]
-                    ];
-
-
-                    $menuItemImageQuery = "INSERT INTO images (menuitem_id, image_path, image_name) values (:menuitem_id, :image_path, :image_name)";
-                    $menuItemImageStatement = $db->prepare($menuItemImageQuery);
-                    $menuItemImageStatement->bindValue(':menuitem_id', $menuItemID);
-                    $menuItemImageStatement->bindValue(':image_name', $imageName);
-
-                    foreach($resized_paths as $resized_path){
-                        $menuItemImageStatement->bindValue(':image_path', $resized_path['image_path']);
-                        $menuItemImageStatement->execute();
+                    if(file_is_an_image($temporary_file_path, $new_file_path)){
+                        $menuItemQuery = "INSERT INTO menuitems (item_name, description, cost, category_id) values (:item_name, :description, :cost, :category_id)";
+                        $menuItemStatement = $db->prepare($menuItemQuery);
+                        $menuItemStatement->bindValue(':item_name', $title);
+                        $menuItemStatement->bindValue(':description', $content);
+                        $menuItemStatement->bindValue(':cost', $validated_cost);
+                        $menuItemStatement->bindValue(':category_id', $validated_categoryId);
+                        $menuItemStatement->execute();
+    
+                        $menuItemID = $db->lastInsertId();
+    
+                        $newPageQuery = "INSERT INTO pages (menuItem_id, page_title) VALUES (:menuItem_id, :page_title)";
+                        $newPageQueryStatement = $db->prepare($newPageQuery);
+                        $newPageQueryStatement->bindValue(':menuItem_id', $menuItemID);
+                        $newPageQueryStatement->bindValue(':page_title', $title);
+                        $newPageQueryStatement->execute();
+    
+                        $resized_paths = [
+                            ['image_path' => $imagePath],
+                            ['image_path' => 'uploads/' . $imageName . '_medium' . '.' . $imageExtension],
+                            ['image_path' => 'uploads/' . $imageName . '_thumbnail' . '.' . $imageExtension]
+                        ];
+    
+                        $menuItemImageQuery = "INSERT INTO images (menuitem_id, image_path, image_name) values (:menuitem_id, :image_path, :image_name)";
+                        $menuItemImageStatement = $db->prepare($menuItemImageQuery);
+                        $menuItemImageStatement->bindValue(':menuitem_id', $menuItemID);
+                        $menuItemImageStatement->bindValue(':image_name', $imageName);
+    
+                        foreach($resized_paths as $resized_path){
+                            $menuItemImageStatement->bindValue(':image_path', $resized_path['image_path']);
+                            $menuItemImageStatement->execute();
+                        }
+                        
+                        header("Location: menu.php");
                     }
-                    
-                    header("Location: menu.php");
+
                 }
             }
         }
     }
 }
 
-// Checks to see if the file uploaded is an image then moves it to the uploads subfolder.
+// Builds a path string that uses slashes appropriate for our OS.
+function file_upload_path($original_filename, $upload_subfolder_name = 'uploads'){
+    // Getting the name of the current folder
+    $current_folder = dirname(__FILE__);
+
+    // Build an array of paths segment names to be joins using OS specific slashes.
+    $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+
+    // The DIRECTORY_SEPARATOR constant is OS specific.
+    return join(DIRECTORY_SEPARATOR, $path_segments);
+}
+
+// Checks to see if the file uploaded is an image/pdf then moves it to the uploads subfolder.
 function file_is_an_image($temporary_path, $new_path){
     $allowed_file_mime_types = ['image/jpeg', 'image/png'];
     $allowed_file_extensions = ['jpg', 'jpeg', 'png'];
@@ -97,23 +113,41 @@ function file_is_an_image($temporary_path, $new_path){
 
     $file_upload_detected = (isset($_FILES['file']) && $_FILES['file']['error'] === 0);
     $upload_error_detected = (isset($_FILES['file']) && $_FILES['file']['error'] > 0);
+    $errorMessage = "";
 
     if($file_upload_detected){
+        $filename = $_FILES['file']['name'];
+        $temporary_file_path = $_FILES['file']['tmp_name'];
+        
+        $actual_image_extension = pathinfo($filename, PATHINFO_EXTENSION);
+        $actual_image_mime_type = mime_content_type($_FILES['file']['tmp_name']);
+        $allowed_image_mime_types = ['image/gif', 'image/jpeg', 'image/png'];
+
+        // Moves the uploaded file to the uploads folder
+        $new_file_path = file_upload_path($filename);
+        if(file_is_an_image($temporary_file_path, $new_file_path)){
+            move_uploaded_file($temporary_file_path, $new_file_path);
+        }else{
+            $errorMessage = "Only JPG, JPEG, PNG files are allowed.";
+        }
+
         // Checks the uploaded image for "image-ness" 
         // If valid, it duplicates and resizes the image to 400px wide for the medium version
         // and 50px wide for the thumbnail version, then saves both to the uploads folder.
-        $image_name = pathinfo($_FILES['file']['name'], PATHINFO_FILENAME);
+        if(in_array($actual_image_mime_type, $allowed_image_mime_types)){
+            $image_name = pathinfo($_FILES['file']['name'], PATHINFO_FILENAME);
 
-        $mediumImage = new ImageResize($new_file_path);
-        $mediumImage->quality_png = 100;
-        $mediumImage->crop(400, 300);
-        $mediumImage_filePath = 'uploads/' . $image_name . '_medium' . '.' . $actual_image_extension;
-        $mediumImage->save($mediumImage_filePath);
+            $mediumImage = new ImageResize($new_file_path);
+            $mediumImage->quality_png = 100;
+            $mediumImage->crop(400, 300);
+            $mediumImage_filePath = 'uploads/' . $image_name . '_medium' . '.' . $actual_image_extension;
+            $mediumImage->save($mediumImage_filePath);
 
-        $thumbnailImage = new ImageResize($new_file_path);
-        $thumbnailImage->resizeToWidth(50);
-        $thumbnailImage_filePath = 'uploads/' . $image_name . '_thumbnail' . '.' . $actual_image_extension;
-        $thumbnailImage->save($thumbnailImage_filePath);
+            $thumbnailImage = new ImageResize($new_file_path);
+            $thumbnailImage->resizeToWidth(50);
+            $thumbnailImage_filePath = 'uploads/' . $image_name . '_thumbnail' . '.' . $actual_image_extension;
+            $thumbnailImage->save($thumbnailImage_filePath);
+        }
     }
 ?>
 
@@ -130,7 +164,7 @@ function file_is_an_image($temporary_path, $new_path){
 <body>
 <?php
 if (isset($_SESSION['nonAdminUser'])) {
-    header("Location: home.php");
+    header("Location: menu.php");
 } else {
     ?>
     <h1>New Page Post</h1>
